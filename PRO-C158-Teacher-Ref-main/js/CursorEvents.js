225cafb0e47ae8b7c08b8b7d1f7d440c3a5a3ec3
yashwaerdhan <68593331+yashwaerdhan@users.noreply.github.com>
2023-07-23 15:05:00 +0530

    Add files via upload

---
+++ PRO-C158-Teacher-Ref-main/js/CursorEvents.js
@@ -0,0 +1,71 @@
+AFRAME.registerComponent("cursor-listener", {
+  schema: {
+    selectedItemId: { default: "", type: "string" },
+  },
+  init: function () {
+    this.handleClickEvents();
+    this.handleMouseEnterEvents();
+    this.handleMouseLeaveEvents();
+  },
+
+  handleClickEvents:function(){
+    this.el.addEventListener("click",evt=>{
+      const placesContainer=document.querySelector("#places-container")
+      const {state}=placesContainer.getAttribute("tour")
+
+      if(state==="places-list"){
+        const id=this.el.getAttribute("id");
+        const placesId=[
+          "taj-mahal",
+          "budapest",
+          "new-york-city",
+          "eiffel-tower"
+        ];
+        if(placesId.includes(id)){
+          placesContainer.setAttribute("tour",{
+            state:"view",
+            selectedCard:id
+          })
+        }
+      }
+    })
+  },
+
+
+  handlePlacesListState: function () {
+    const id = this.el.getAttribute("id");
+    const placesId = ["taj-mahal", "budapest", "new-york-city", "eiffel-tower"];
+    if (placesId.includes(id)) {
+      const placeContainer = document.querySelector("#places-container");
+      placeContainer.setAttribute("cursor-listener", {
+        selectedItemId: id,
+      });
+      this.el.setAttribute("material", {
+        color: "#D76B30",
+        opacity: 1,
+      });
+    }
+  },
+  handleMouseEnterEvents: function () {
+    //Cursor 'mouseenter' Events
+    this.el.addEventListener("mouseenter", () => {
+      this.handlePlacesListState();
+    });
+  },
+  handleMouseLeaveEvents: function () {
+    //Cursor 'mouseleave' Events
+    this.el.addEventListener("mouseleave", () => {
+      const { selectedItemId } = this.data;
+      if (selectedItemId) {
+        const el = document.querySelector(`#${selectedItemId}`);
+        const id = el.getAttribute("id");
+        if (id == selectedItemId) {
+          el.setAttribute("material", {
+            color: "#0077CC",
+            opacity: 1,
+          });
+        }
+      }
+    });
+  },
+});
---
+++ PRO-C158-Teacher-Ref-main/js/Tour.js
@@ -0,0 +1,129 @@
+AFRAME.registerComponent("tour", {
+  schema:{
+    state:{type:"string",default:"places-list"},
+    selectedCard:{
+      type:"string",default:"#card1"
+    },
+  },
+  showView:function(){
+    const {selectedCard}=this.data;
+    const skyEl=document.querySelector("#main-container");
+    skyEl.setAttribute("material",{
+      src:`./assets/thumbnails/360_images/${selectedCard}/place-0.jpg`,
+      color:"#fff"
+    })
+  },
+  tick:function(){
+    const {state}=this.el.getAttribute("tour")
+    if(state==="view"){
+      this.hideEl([this.placesContainer])
+      this.showView()
+    }
+  },
+
+  init: function () {
+    this.placesContainer = this.el;
+    this.createCards();
+  },
+
+  hideEl:function(elList){
+    elList.map(el=>{
+      el.setAttribute("visible",false)
+    })
+  },
+  createCards: function () {
+    const thumbNailsRef = [
+      {
+        id: "taj-mahal",
+        title: "Taj Mahal",
+        url: "./assets/thumbnails/taj_mahal.png",
+      },
+      {
+        id: "budapest",
+        title: "Budapest",
+        url: "./assets/thumbnails/budapest.jpg",
+      },
+
+      {
+        id: "eiffel-tower",
+        title: "Eiffel Tower",
+        url: "./assets/thumbnails/eiffel_tower.jpg",
+      },
+      {
+        id: "new-york-city",
+        title: "New York City",
+        url: "./assets/thumbnails/new_york_city.png",
+      },
+    ];
+    let prevoiusXPosition = -60;
+
+    for (var item of thumbNailsRef) {
+      const posX = prevoiusXPosition + 25;
+      const posY = 10;
+      const posZ = -40;
+      const position = { x: posX, y: posY, z: posZ };
+      prevoiusXPosition = posX;
+
+      // Border Element
+      const borderEl = this.createBorder(position, item.id);
+
+      // Thumbnail Element
+      const thumbNail = this.createThumbNail(item);
+      borderEl.appendChild(thumbNail);
+
+      // Title Text Element
+      const titleEl = this.createTitleEl(position, item);
+      borderEl.appendChild(titleEl);
+
+      this.placesContainer.appendChild(borderEl);
+    }
+  },
+  createBorder: function (position, id) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("id", id);
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "ring",
+      radiusInner: 9,
+      radiusOuter: 10,
+    });
+    entityEl.setAttribute("position", position);
+    entityEl.setAttribute("material", {
+      color: "#0077CC",
+      opacity: 1,
+    });
+
+    //Add cursor-listener component to the ring border entity to change it's color 
+    //On Cursor 'mouseenter' and 'mouseleave' entity
+    entityEl.setAttribute("cursor-listener", {});
+
+    return entityEl;
+  },
+  createThumbNail: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "circle",
+      radius: 9,
+    });
+    entityEl.setAttribute("material", { src: item.url });
+
+    return entityEl;
+  },
+  createTitleEl: function (position, item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("text", {
+      font: "exo2bold",
+      align: "center",
+      width: 70,
+      color: "#e65100",
+      value: item.title,
+    });
+    const elPosition = position;
+    elPosition.y = -20;
+    entityEl.setAttribute("position", elPosition);
+    entityEl.setAttribute("visible", true);
+    
+    return entityEl;
+  },
+});
